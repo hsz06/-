47057af76ec68f6a1d716619dc8eda657f794206
TWCjiang <1457958368@qq.com>
2023-05-24 17:03:53 +0800

    2000-2022自然灾害数据统计

---
+++ Global_natural_disasters.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import *
+
+df = pd.read_csv('Disaster.csv', encoding='utf-8')
+a = df.describe()
+# 统计各年自然灾害总数
+count1 = df['Year'].value_counts(sort=False)
+count1 = list(count1)
+theYear = []
+i = 0
+nowYear = 2000
+
+while i < len(count1):
+    theYear.append(nowYear)
+    i += 1
+    nowYear += 1
+print(theYear)
+y1_min = np.argmin(count1)
+y1_max = np.argmax(count1)
+
+plt.figure(figsize=(20, 10), dpi=100)
+plt.plot(theYear, count1, c='red')
+plt.scatter(theYear, count1, c='red')
+y_ticks = range(1)
+show_min = '[' + str(y1_min + 2000) + ',' + str(count1[y1_min]) + ']'
+show_max = '[' + str(y1_max + 2000) + ',' + str(count1[y1_max]) + ']'
+plt.plot(y1_min + 2000, count1[y1_min], 'ko')
+plt.plot(y1_max + 2000, count1[y1_max], 'ko')
+plt.annotate(show_min, xy=((y1_min + 2000), count1[y1_min]), xytext=((y1_min + 2000), count1[y1_min]))
+plt.annotate(show_max, xy=((y1_max + 2000), count1[y1_max]), xytext=((y1_max + 2000), count1[y1_max]))
+plt.xlabel("year", fontdict={'size': 16})
+plt.ylabel("total number", fontdict={'size': 16})
+plt.title("Total number of natural disasters per year ", fontdict={'size': 20})
+plt.show()
