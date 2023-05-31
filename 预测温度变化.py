a7710be34fe42f14e0c8724467d5ae82fb158ee3
hsz06 <1261081994@qq.com>
2023-05-31 20:56:46 +0800

    灾害预测

--- 预测温度变化.py
+++ 预测温度变化.py
@@ -58,8 +58,8 @@ from sklearn.linear_model import LinearRegression
 temper = np.array([0.22797156398104265, 0.2064306220095694, 0.6108038277511961, 0.6300094786729858, 0.2877061611374408, 0.5422019230769232, 0.9828018867924528, 0.7521, 0.6747867298578198, 0.8624258373205742, 0.9297264150943395, 0.8450093023255815, 0.7737476635514018, 0.8474225352112676, 0.8562638888888889, 1.011589861751152, 0.7962394366197183, 0.9020654205607477, 1.0877906976744185, 0.8039953917050691, 0.8874205607476636, 0.917185185185185, 1.0961203703703704, 1.2529814814814815, 1.4279906976744186, 1.2699392523364488, 1.2881018518518519, 1.4276279069767441, 1.538565420560748, 1.3170093457943928])
 
 #线性回归预测
-g = sns.heatmap([warm_gas,].corr(),annot=True, cmap = "coolwarm")
-plt.show()
+#g = sns.heatmap([warm_gas,].corr(),annot=True, cmap = "coolwarm")
+#plt.show()
 # 构建特征矩阵和目标向量
 temper1=temper[:-1]
 X = np.arange(1, len(temper1)+1).reshape(-1, 1)
@@ -118,12 +118,12 @@ model.fit(X, y)
 
 # 预测未来三年温度变化
 future_X = np.column_stack((future_warm_gas, future_percent))
-future_temper = model.predict(future_X)
+future_temper1 = model.predict(future_X)
 
 print("未来三年温度变化预测：")
 j=2021
-for i in range(len(future_temper)):
-    print(f"第{j}年预测温度变化：{future_temper[i]}")
+for i in range(len(future_temper1)):
+    print(f"第{j}年预测温度变化：{future_temper1[i]}")
     j=j+1
 #根据21年数据判断模型，最终选择根据当年数据预测温度变化比较契合
 '''
@@ -151,3 +151,182 @@ for i in range(len(future_temper)):
 系统动态性：温度变化受多种因素影响，不仅仅取决于当年的温室气体排放和能源转型，还与其他因素如天气、自然灾害等有关，因此仅凭当年数据预测明年温度变化可能存在一定的局限性。
 综上所述，根据当年的温室气体排放和能源转型预测当年温度变化和明年温度变化具有直接关联和实时性的优势，能够及时反映当年的温度变化趋势，所以最终选择第一种方法
 '''
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import *
+
+df = pd.read_csv('Disaster.csv', encoding='utf-8', index_col=0)
+a = df['Disaster Type'].info()
+print(a)
+#根据灾害类型筛选与全球变暖相关的自然灾害
+#洪水
+flag=df['Disaster Type']=='Flood'
+Flood=df[flag]
+#print(Flood.info())
+count1 = Flood['Year'].value_counts(sort=False)
+flood=list(count1)
+#求平均值，
+avg=average(flood)
+#处理洪水数量，以平均值作为判定标准，大于平均值为1是易于发生，小于为0不易于发生
+for i in range(len(flood)):
+    if flood[i]>=avg:
+        flood[i]=1
+    else:
+        flood[i]=0
+
+#极端温度
+flag=df['Disaster Type']=='Extreme temperature'
+Extreme_temperature=df[flag]
+#print(Flood.info())
+Extreme_temperature_count2 = Extreme_temperature['Year'].value_counts(sort=False)
+#print(Extreme_temperature_count2)
+Extreme_temperature=list(Extreme_temperature_count2)
+#求平均值，
+avg=average(Extreme_temperature)
+#处理特征值，以平均值作为判定标准，大于平均值为1是易于发生，小于为0不易于发生
+for i in range(len(Extreme_temperature)):
+    if Extreme_temperature[i]>=avg:
+        Extreme_temperature[i]=1
+    else:
+        Extreme_temperature[i]=0
+
+#干旱
+flag=df['Disaster Type']=='Drought'
+Drought=df[flag]
+#print(Flood.info())
+Droughtcount1 = Drought['Year'].value_counts(sort=False)
+#print(Droughtcount1)
+Drought=list(Droughtcount1)
+#求平均值，
+avg=average(Drought)
+#处理特征值，以平均值作为判定标准，大于平均值为1是易于发生，小于为0不易于发生
+for i in range(len(Drought)):
+    if Drought[i]>=avg:
+        Drought[i]=1
+    else:
+        Drought[i]=0
+
+#野火
+flag=df['Disaster Type']=='Wildfire'
+Wildfire=df[flag]
+#print(Flood.info())
+Wildfirecount1 = Wildfire['Year'].value_counts(sort=False)
+#print(Wildfirecount1)
+Wildfire=list(Wildfirecount1)
+#求平均值，
+avg=average(Wildfire)
+#处理特征值，以平均值作为判定标准，大于平均值为1是易于发生，小于为0不易于发生
+for i in range(len(Wildfire)):
+    if Wildfire[i]>=avg:
+        Wildfire[i]=1
+    else:
+        Wildfire[i]=0
+
+#飓风
+flag=df['Disaster Type']=='Storm'
+Storm=df[flag]
+#print(Flood.info())
+Stormcount1 = Storm['Year'].value_counts(sort=False)
+storm=list(Stormcount1)
+#求平均值，
+avg=average(storm)
+#处理特征值，以平均值作为判定标准，大于平均值为1是易于发生，小于为0不易于发生
+for i in range(len(storm)):
+    if storm[i]>=avg:
+        storm[i]=1
+    else:
+        storm[i]=0
+model = LinearRegression()
+
+
+# 构建特征矩阵,选取温室气体排放，能源转型数据和温度变换数据
+X = np.column_stack((warm_gas[1:], percent[10:],temper[18:-1]))
+y = flood[10:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_warm_gas, future_percent,future_temper))
+floodfuture = model.predict(future_X)
+print(f"洪水：{floodfuture}")
+#极端温度
+X = np.column_stack((warm_gas[1:], percent[10:],temper[18:-1]))
+y = Extreme_temperature[10:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_warm_gas, future_percent,future_temper))
+Extreme_temperaturefut = model.predict(future_X)
+print(f"极端温度：{Extreme_temperaturefut}")
+
+#干旱
+X = np.column_stack((warm_gas[1:], percent[10:],temper[18:-1]))
+y = Drought[10:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_warm_gas, future_percent,future_temper))
+Droughtfut = model.predict(future_X)
+print(f"干旱：{Droughtfut }")
+
+#野火
+X = np.column_stack((warm_gas[1:], percent[10:],temper[18:-1]))
+y = Wildfire[10:21]
+
+# 拟合模型
+model.fit(X, y)
+
+future_X = np.column_stack((future_warm_gas, future_percent,future_temper))
+Wildfirefut = model.predict(future_X)
+print(f"野火：{Wildfirefut }")
+
+#飓风
+X = np.column_stack((warm_gas[1:], percent[10:],temper[18:-1]))
+y = storm[10:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_warm_gas, future_percent,future_temper))
+stormfut = model.predict(future_X)
+print(f"飓风：{stormfut }")
+
+# 构建特征矩阵,选取能源转型数据和温度变换数据
+X = np.column_stack((percent,temper[8:-1]))
+y = flood[:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_percent,future_temper))
+floodfuture = model.predict(future_X)
+print(f"洪水：{floodfuture}")
+
+#极端温度
+X = np.column_stack((percent,temper[8:-1]))
+y = Extreme_temperature[:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_percent,future_temper))
+Extreme_temperaturefut = model.predict(future_X)
+print(f"极端温度：{Extreme_temperaturefut}")
+
+#干旱
+X = np.column_stack((percent,temper[8:-1]))
+y = Drought[:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_percent,future_temper))
+Droughtfut = model.predict(future_X)
+print(f"干旱：{Droughtfut }")
+
+#野火
+X = np.column_stack((percent,temper[8:-1]))
+y = Wildfire[:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_percent,future_temper))
+Wildfirefut = model.predict(future_X)
+print(f"野火：{Wildfirefut }")
+
+#飓风
+X = np.column_stack((percent,temper[8:-1]))
+y = storm[:21]
+# 拟合模型
+model.fit(X, y)
+future_X = np.column_stack((future_percent,future_temper))
+stormfut = model.predict(future_X)
+print(f"飓风：{stormfut }")
